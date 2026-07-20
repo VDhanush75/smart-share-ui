fa5084ece774fb14ab11954a89d1d786260976cd
v dhanush <dhanushdhanush76593@gmail.com>
2026-07-20 21:28:29 +0530

    refactor: remove console logs and update routeTree with type declarations

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -8,7 +8,6 @@ if (!supabaseUrl || !supabaseAnonKey) {
     "Missing Supabase environment variables. Make sure VITE_SUPABASE_URL and VITE_SUPABASE_ANON_KEY are set."
   );
 }
-console.log(import.meta.env.VITE_SUPABASE_URL)
-console.log(import.meta.env.VITE_SUPABASE_ANON_KEY)
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
